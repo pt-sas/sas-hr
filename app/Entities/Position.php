988f4e89e61a3a43fd37e7792e76465759e97d2c
Hatsu17 <keihatsu17@gmail.com>
2023-12-07 14:32:14 +0700

    Add Menu Position

---
+++ app/Entities/Position.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Entities;
+
+use CodeIgniter\Entity;
+
+class Position extends Entity
+{
+    protected $md_position_id;
+    protected $value;
+    protected $name;
+    protected $description;
+    protected $isactive;
+    protected $created_by;
+    protected $updated_by;
+
+    protected $dates   = [
+        'created_at',
+        'updated_at',
+    ];
+
+    public function getPositionId()
+    {
+        return $this->attributes['md_position_id'];
+    }
+
+    public function setPositionId($md_position_id)
+    {
+        $this->attributes['md_position_id'] = $md_position_id;
+    }
+
+    public function getValue()
+    {
+        return $this->attributes['value'];
+    }
+
+    public function setValue($value)
+    {
+        $this->attributes['value'] = $value;
+    }
+
+    public function getName()
+    {
+        return $this->attributes['name'];
+    }
+
+    public function setName($name)
+    {
+        $this->attributes['name'] = $name;
+    }
+
+    public function getDescription()
+    {
+        return $this->attributes['description'];
+    }
+
+    public function setDescription($description)
+    {
+        $this->attributes['description'] = $description;
+    }
+
+    public function getIsActive()
+    {
+        return $this->attributes['isactive'];
+    }
+
+    public function setIsActive($isactive)
+    {
+        return $this->attributes['isactive'] = $isactive;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->attributes['created_at'];
+    }
+
+    public function getCreatedBy()
+    {
+        return $this->attributes['created_by'];
+    }
+
+    public function setCreatedBy($created_by)
+    {
+        $this->attributes['created_by'] = $created_by;
+    }
+
+    public function getUpdatedAt()
+    {
+        return $this->attributes['updated_at'];
+    }
+
+    public function getUpdatedBy()
+    {
+        return $this->attributes['updated_by'];
+    }
+
+    public function setUpdatedBy($updated_by)
+    {
+        $this->attributes['updated_by'] = $updated_by;
+    }
+}
