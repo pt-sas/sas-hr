01e7115bd5770a0f4d10b35973c344882648ac76
Tintapen <okiper12@gmail.com>
2024-01-10 16:12:40 +0700

    Modify table allowance

--- app/Database/Migrations/2023-12-13-061132_CreateTableTrxTkh.php
+++ app/Database/Migrations/2023-12-13-061132_CreateTableTrxTkh.php
@@ -11,15 +11,18 @@ class CreateTableTrxTkh extends Migration
     public function up()
     {
         $this->forge->addField([
-            'trx_allow_attendance_id'   => ['type' => 'INT', 'constraint' => 6, 'null' => false, 'auto_increment' => true],
+            'trx_allow_attendance_id'   => ['type' => 'INT', 'constraint' => 11, 'null' => false, 'auto_increment' => true],
             'isactive'              => ['type' => 'CHAR', 'constraint' => 1, 'null' => false, 'default' => 'Y'],
             'created_at'            => ['type' => 'timestamp default current_timestamp'],
-            'created_by'            => ['type' => 'INT', 'constraint' => 6, 'null' => false],
+            'created_by'            => ['type' => 'INT', 'constraint' => 11, 'null' => false],
             'updated_at'            => ['type' => 'timestamp default current_timestamp'],
-            'updated_by'            => ['type' => 'INT', 'constraint' => 6, 'null' => false],
-            'record_id'             => ['type' => 'INT', 'constraint' => 6, 'null' => false],
-            'submissiontype'        => ['type' => 'INT', 'constraint' => 6, 'null' => false],
-            'amount'                => ['type' => 'DECIMAL', 'constraint' => 10, 'null' => false],
+            'updated_by'            => ['type' => 'INT', 'constraint' => 11, 'null' => false],
+            'record_id'             => ['type' => 'INT', 'constraint' => 11, 'null' => false],
+            'table'                 => ['type' => 'VARCHAR', 'constraint' => 32, 'null' => false],
+            'submissiondate'        => ['type' => 'timestamp default current_timestamp'],
+            'submissiontype'        => ['type' => 'VARCHAR', 'constraint' => 40, 'null' => false],
+            'md_employee_id'        => ['type' => 'INT', 'constraint' => 11, 'null' => false],
+            'amount'                => ['type' => 'FLOAT'],
             'description'           => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => false]
         ]);
         $this->forge->addKey('trx_allow_attendance_id', true);
