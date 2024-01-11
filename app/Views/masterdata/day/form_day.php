d26f015f3744876630ef5af4f2df46e4a59f0230
Hatsu17 <keihatsu17@gmail.com>
2024-01-11 13:20:06 +0700

    AddMasterDataDay

---
+++ app/Views/masterdata/day/form_day.php
@@ -0,0 +1,33 @@
+<div class="card-body card-form">
+    <form class="form-horizontal" id="form_day">
+        <?= csrf_field(); ?>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="value">Kode Hari <span class="required">*</span></label>
+                    <input type="text" class="form-control code" id="value" name="value" readonly>
+                    <small class="form-text text-danger" id="error_value"></small>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="name">Nama <span class="required">*</span></label>
+                    <input type="text" class="form-control" id="name" name="name">
+                    <small class="form-text text-danger" id="error_name"></small>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="description">Deskripsi </label>
+                    <textarea type="text" class="form-control" id="description" name="description" rows="2"></textarea>
+                </div>
+                <div class="form-check">
+                    <label class="form-check-label">
+                        <input type="checkbox" class="form-check-input active" id="isactive" name="isactive">
+                        <span class="form-check-sign">Aktif</span>
+                    </label>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
