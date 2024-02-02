215a686650d5ba6270f4f5b4cae90e95a047cce6
Hatsu17 <keihatsu17@gmail.com>
2024-02-02 13:20:02 +0700

    Add Logic JS

---
+++ public/custom/js/Logic.js
@@ -0,0 +1,75 @@
+$(".timepicker").datetimepicker({
+  format: "HH:mm",
+  useCurrent: false,
+});
+
+$(".timepicker-start").datetimepicker({
+  format: "HH:mm",
+  useCurrent: false,
+});
+
+$(".timepicker-end").datetimepicker({
+  format: "HH:mm",
+  useCurrent: false,
+});
+
+$(".timepicker-start").on("dp.change", function (e) {
+  $(".timepicker-end").data("DateTimePicker").minDate(e.date);
+});
+
+$(".date-start").datetimepicker({
+  format: "DD-MMM-YYYY",
+  showTodayButton: true,
+  showClear: true,
+  showClose: true,
+  useCurrent: false,
+});
+
+$(".date-end").datetimepicker({
+  format: "DD-MMM-YYYY",
+  showTodayButton: true,
+  showClear: true,
+  showClose: true,
+  useCurrent: false,
+});
+
+$("#form_official_permission").on(
+  "change dp.change",
+  "select[name=md_leavetype_id], input[name=startdate]",
+  function (e) {
+    let _this = $(this);
+    const form = _this.closest("form");
+    let value = this.value;
+    let formData = new FormData();
+
+    let url = ADMIN_URL + "ijin-resmi/getEndDate";
+
+    formData.append(this.name, value);
+
+    if (this.name === "md_leavetype_id")
+      formData.append("startdate", form.find("[name=startdate]").val());
+
+    if (this.name === "startdate")
+      formData.append(
+        "md_leavetype_id",
+        form.find("[name=md_leavetype_id]").val()
+      );
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      processData: false,
+      contentType: false,
+      cache: false,
+      dataType: "JSON",
+      success: function (result) {
+        console.log(result);
+        if (form.find("[name=startdate]").val() !== "") {
+          form.find("[name=enddate]").val(moment(result).format("DD-MMM-Y"));
+        } else {
+        }
+      },
+    });
+  }
+);
