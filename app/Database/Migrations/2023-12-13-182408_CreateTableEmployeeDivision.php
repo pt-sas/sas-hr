73b243fc7f375ce5cd51f72c5bad1006aa7eac27
Hatsu17 <keihatsu17@gmail.com>
2024-02-02 13:23:02 +0700

    -

--- app/Database/Migrations/2023-12-13-182408_CreateTableEmployeeDivision.php
+++ app/Database/Migrations/2023-12-13-182408_CreateTableEmployeeDivision.php
@@ -16,13 +16,18 @@ class CreateTableEmployeeDivision extends Migration
             'created_at'            => ['type' => 'timestamp default current_timestamp'],
             'created_by'            => ['type' => 'INT', 'constraint' => 11, 'null' => false],
             'updated_at'            => ['type' => 'timestamp default current_timestamp'],
-            'updated_by'            => ['type' => 'INT', 'constraint' => 11, 'null' => false],
-            'md_employee_id'        => ['type' => 'INT', 'constraint' => 11, 'null' => false],
-            'md_division_id'          => ['type' => 'INT', 'constraint' => 11, 'null' => false],
+            'updated_by'            => ['type' => 'INT', 'constraint' => 6, 'null' => false],
+            'md_employee_id'        => ['type' => 'INT', 'constraint' => 6, 'null' => false],
+            'table'                 => ['type' => 'VARCHAR', 'constraint' => 32, 'null' => false],
+            'record_id'             => ['type' => 'INT', 'constraint' => 6, 'null' => false],
+            'submissiontype'        => ['type' => 'INT', 'constraint' => 6, 'null' => false],
+            'allowancedate'        => ['type' => 'timestamp', 'null' => false],
+            'amount'                => ['type' => 'DECIMAL', 'constraint' => 10, 'null' => false],
             'description'           => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => false]
         ]);
-        $this->forge->addKey('md_employee_division_id', true);
-        $this->forge->createTable('md_employee_division', true);
+        $this->forge->addKey('trx_allow_attendance_id', true);
+        $this->forge->addKey('md_employee_id', false);
+        $this->forge->createTable('trx_allow_attendance', true);
     }
 
     public function down()
