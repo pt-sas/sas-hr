313616a7c243fdfcd328274b2d7a3ff291ee5ba1
Hatsu17 <keihatsu17@gmail.com>
2024-02-13 13:46:38 +0700

    Cancel Fixing

--- app/Config/Validation.php
+++ app/Config/Validation.php
@@ -593,6 +593,16 @@ class Validation
                 'required'      => 'Mohon mengisi {field} dahulu'
             ]
         ],
+        'image'                 => [
+            'label'             => 'gambar',
+            'rules'             => 'uploaded[image]|max_size[image, 3024]|is_image[image]|mime_in[image,image/jpg,image/jpeg,image/png]',
+            'errors'            => [
+                'uploaded'      => 'Mohon upload {field} dahulu',
+                'max_size'      => 'Data {field} melebehi batas maksimum 3 Mb',
+                'is_image'      => 'Format file {field} salah',
+                'mime_in'       => 'Format file {field} wajib {param}',
+            ]
+        ],
     ];
 
     public $pengajuan = [
