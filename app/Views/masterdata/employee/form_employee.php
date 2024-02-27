eef3861a1a27cf0766cc6191024462ba88f8af5f
Hatsu17 <keihatsu17@gmail.com>
2024-02-27 15:06:02 +0700

    Add Funtion to get Employee on Model Employee

--- app/Views/masterdata/employee/form_employee.php
+++ app/Views/masterdata/employee/form_employee.php
@@ -37,7 +37,8 @@
                 </div>
                 <div class="form-upload">
                     <label class="col-md-6 form-upload-foto" id="product-upload">
-                        <input type="file" class="control-upload-image" id="image" name="image" onchange="previewImage(this)" accept="image/jpeg, image/png"></input>
+                        <input type="file" class="control-upload-image" id="image" name="image"
+                            onchange="previewImage(this)" accept="image/jpeg, image/png"></input>
                         <img class="img-upload" src="<?= base_url('custom/image/cameraroll.png') ?>" />
                     </label>
                     <small class="form-text text-danger" id="error_image"></small>
@@ -82,7 +83,8 @@
         <div class="col-md-3">
             <div class="form-group">
                 <label for="md_bloodtype_id">Golongan Darah </label>
-                <select class="form-control select-data" id="md_bloodtype_id" name="md_bloodtype_id" data-url="bloodtype/getList">
+                <select class="form-control select-data" id="md_bloodtype_id" name="md_bloodtype_id"
+                    data-url="bloodtype/getList">
                     <option value="">Select Golongan Darah</option>
                 </select>
             </div>
@@ -99,10 +101,10 @@
             <div class="form-check">
                 <label>Jenis Kelamin <span class="required">*</span></label><br />
                 <?php foreach ($ref_list as $row) : ?>
-                    <label class="form-radio-label">
-                        <input class="form-radio-input" type="radio" name="gender" value="<?= $row->value ?>">
-                        <span class="form-radio-sign"><?= $row->name ?></span>
-                    </label>
+                <label class="form-radio-label">
+                    <input class="form-radio-input" type="radio" name="gender" value="<?= $row->value ?>">
+                    <span class="form-radio-sign"><?= $row->name ?></span>
+                </label>
                 <?php endforeach; ?>
                 <small class="form-text text-danger" id="error_gender"></small>
             </div>
@@ -110,7 +112,8 @@
         <div class="col-md-3">
             <div class="form-group">
                 <label for="nationality">Kewarganegaraan <span class="required">*</span></label>
-                <select class="form-control select-data" id="nationality" name="nationality" data-url="reference/getList/$Nationality">
+                <select class="form-control select-data" id="nationality" name="nationality"
+                    data-url="reference/getList/$Nationality">
                     <option value="">Select Kewarganegaraan</option>
                 </select>
                 <small class="form-text text-danger" id="error_nationality"></small>
@@ -119,7 +122,8 @@
         <div class="col-md-3">
             <div class="form-group">
                 <label for="md_religion_id">Agama <span class="required">*</span></label>
-                <select class="form-control select-data" id="md_religion_id" name="md_religion_id" data-url="religion/getList">
+                <select class="form-control select-data" id="md_religion_id" name="md_religion_id"
+                    data-url="religion/getList">
                     <option value="">Select Agama</option>
                 </select>
                 <small class="form-text text-danger" id="error_md_religion_id"></small>
@@ -128,7 +132,8 @@
         <div class="col-md-3">
             <div class="form-group">
                 <label for="marital_status">Status Menikah <span class="required">*</span></label>
-                <select class="form-control select-data" id="marital_status" name="marital_status" data-url="reference/getList/$MaritalStatus">
+                <select class="form-control select-data" id="marital_status" name="marital_status"
+                    data-url="reference/getList/$MaritalStatus">
                     <option value="">Select Status Menikah</option>
                 </select>
                 <small class="form-text text-danger" id="error_marital_status"></small>
@@ -156,7 +161,8 @@
         <div class="col-md-6">
             <div class="form-group">
                 <label for="homestatus">Status Rumah <span class="required">*</span></label>
-                <select class="form-control select-data" id="homestatus" name="homestatus" data-url="reference/getList/$HomeStatus">
+                <select class="form-control select-data" id="homestatus" name="homestatus"
+                    data-url="reference/getList/$HomeStatus">
                     <option value="">Select Status Rumah</option>
                 </select>
                 <small class="form-text text-danger" id="error_homestatus"></small>
@@ -167,9 +173,10 @@
             <div class="form-group">
                 <label for="md_branch_id">Cabang <span class="required">*</span></label>
                 <div class="select2-input select2-primary">
-                    <select class="form-control multiple-select" id="md_branch_id" name="md_branch_id" multiple="multiple" style="width: 100%;">
+                    <select class="form-control multiple-select" id="md_branch_id" name="md_branch_id"
+                        multiple="multiple" style="width: 100%;">
                         <?php foreach ($branch as $row) : ?>
-                            <option value="<?= $row->getBranchId(); ?>"><?= $row->getName(); ?></option>
+                        <option value="<?= $row->getBranchId(); ?>"><?= $row->getName(); ?></option>
                         <?php endforeach; ?>
                     </select>
                     <small class="form-text text-danger" id="error_md_branch_id"></small>
@@ -180,9 +187,10 @@
             <div class="form-group">
                 <label for="md_division_id">Divisi <span class="required">*</span></label>
                 <div class="select2-input select2-primary">
-                    <select class="form-control multiple-select" id="md_division_id" name="md_division_id" multiple="multiple" style="width: 100%;">
+                    <select class="form-control multiple-select" id="md_division_id" name="md_division_id"
+                        multiple="multiple" style="width: 100%;">
                         <?php foreach ($division as $row) : ?>
-                            <option value="<?= $row->getDivisionId(); ?>"><?= $row->getName(); ?></option>
+                        <option value="<?= $row->getDivisionId(); ?>"><?= $row->getName(); ?></option>
                         <?php endforeach; ?>
                     </select>
                     <small class="form-text text-danger" id="error_md_division_id"></small>
@@ -191,8 +199,9 @@
         </div>
         <div class="col-md-6">
             <div class="form-group">
-                <label for="superior_id">Superior <span class="required">*</span></label>
-                <select class="form-control select-data" id="superior_id" name="superior_id" data-url="karyawan/superior">
+                <label for="superior_id">Superior</label>
+                <select class="form-control select-data" id="superior_id" name="superior_id"
+                    data-url="karyawan/superior">
                     <option value="">Select Superior</option>
                 </select>
                 <small class="form-text text-danger" id="error_superior_id"></small>
@@ -201,7 +210,8 @@
         <div class="col-md-3">
             <div class="form-group">
                 <label for="md_position_id">Posisi <span class="required">*</span></label>
-                <select class="form-control select-data" id="md_position_id" name="md_position_id" data-url="position/getList">
+                <select class="form-control select-data" id="md_position_id" name="md_position_id"
+                    data-url="position/getList">
                     <option value="">Select Posisi</option>
                 </select>
                 <small class="form-text text-danger" id="error_md_position_id"></small>
@@ -210,7 +220,8 @@
         <div class="col-md-3">
             <div class="form-group">
                 <label for="md_levelling_id">Jabatan <span class="required">*</span></label>
-                <select class="form-control select-data" id="md_levelling_id" name="md_levelling_id" data-url="levelling/getList">
+                <select class="form-control select-data" id="md_levelling_id" name="md_levelling_id"
+                    data-url="levelling/getList">
                     <option value="">Select Jabatan</option>
                 </select>
                 <small class="form-text text-danger" id="error_md_levelling_id"></small>
@@ -238,7 +249,8 @@
         <div class="col-md-3">
             <div class="form-group">
                 <label for="md_status_id">Status Karyawan <span class="required">*</span></label>
-                <select class="form-control select-data" id="md_status_id" name="md_status_id" data-url="status/getList">
+                <select class="form-control select-data" id="md_status_id" name="md_status_id"
+                    data-url="status/getList">
                     <option value="">Select Status</option>
                 </select>
                 <small class="form-text text-danger" id="error_md_status_id"></small>
@@ -258,7 +270,8 @@
         <div class="col-md-6">
             <div class="form-group">
                 <label for="md_country_id">Negara <span class="required">*</span></label>
-                <select class="form-control select-data" id="md_country_dom_id" name="md_country_dom_id" data-url="country/getList">
+                <select class="form-control select-data" id="md_country_dom_id" name="md_country_dom_id"
+                    data-url="country/getList">
                     <option value="">Select Negara</option>
                 </select>
                 <small class="form-text text-danger" id="error_md_country_dom_id"></small>
@@ -310,7 +323,8 @@
         <div class="col-md-6">
             <div class="form-check">
                 <label class="form-check-label">
-                    <input type="checkbox" class="form-check-input" id="issameaddress" name="issameaddress" checked hide-field="address,md_country_id,md_province_id,md_city_id,md_district_id,md_subdistrict_id,postalcode">
+                    <input type="checkbox" class="form-check-input" id="issameaddress" name="issameaddress" checked
+                        hide-field="address,md_country_id,md_province_id,md_city_id,md_district_id,md_subdistrict_id,postalcode">
                     <span class=" form-check-sign">Sama dengan alamat domisili</span>
                 </label>
             </div>
@@ -324,7 +338,8 @@
         <div class="col-md-6">
             <div class="form-group">
                 <label for="md_country_id">Negara <span class="required">*</span></label>
-                <select class="form-control select-data" id="md_country_id" name="md_country_id" data-url="country/getList">
+                <select class="form-control select-data" id="md_country_id" name="md_country_id"
+                    data-url="country/getList">
                     <option value="">Select Negara</option>
                 </select>
             </div>
@@ -393,6 +408,14 @@
                     <option value="tk1">TK/1</option>
                     <option value="tk1">TK/2</option>
                     <option value="tk3">TK/3</option>
+                    <option value="tk1">K/0</option>
+                    <option value="tk1">K/1</option>
+                    <option value="tk1">K/2</option>
+                    <option value="tk3">K/3</option>
+                    <option value="tk1">K/I/0</option>
+                    <option value="tk1">K/I/1</option>
+                    <option value="tk1">K/I/2</option>
+                    <option value="tk3">K/I/3</option>
                 </select>
             </div>
         </div>
@@ -425,7 +448,8 @@
             <div class="form-group">
                 <label for="bpjs_tenaga_periode">Periode </label>
                 <div class="input-group">
-                    <input type="text" class="form-control datepicker" id="bpjs_tenaga_period" name="bpjs_tenaga_period">
+                    <input type="text" class="form-control datepicker" id="bpjs_tenaga_period"
+                        name="bpjs_tenaga_period">
                     <div class="input-group-append">
                         <span class="input-group-text">
                             <i class="fa fa-calendar"></i>
