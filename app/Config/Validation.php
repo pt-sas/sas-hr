6e0070fb54d1b54a23488b1b1aaa03186107e77a
Tintapen <okiper12@gmail.com>
2024-03-04 10:55:08 +0700

    Feat : Add validation upload image2 and image3

--- app/Config/Validation.php
+++ app/Config/Validation.php
@@ -1548,14 +1548,14 @@ class Validation
             'errors'            => [
                 'required'      => 'Mohon pilih {field} dahulu'
             ]
-            ],
-            'startdate'         => [
-                'label'         =>'Tanggal',
-                'rules'         =>'required',
-                'errors'        => [
-                    'required'      => 'Mohon pilih {field} dahulu'
-                ]
+        ],
+        'startdate'         => [
+            'label'         => 'Tanggal',
+            'rules'         => 'required',
+            'errors'        => [
+                'required'      => 'Mohon pilih {field} dahulu'
             ]
+        ]
     ];
 
     public $lembur = [
@@ -1579,14 +1579,14 @@ class Validation
             'errors'            => [
                 'required'      => 'Mohon pilih {field} dahulu'
             ]
-            ],
-            'startdate'         => [
-                'label'         =>'Tanggal',
-                'rules'         =>'required',
-                'errors'        => [
-                    'required'      => 'Mohon pilih {field} dahulu'
-                ]
+        ],
+        'startdate'         => [
+            'label'         => 'Tanggal',
+            'rules'         => 'required',
+            'errors'        => [
+                'required'      => 'Mohon pilih {field} dahulu'
             ]
+        ]
     ];
 
     public $alpa = [
@@ -1638,14 +1638,14 @@ class Validation
             'errors'        => [
                 'required'    => 'Mohon mengisi {field} dahulu'
             ]
-            ],
-            'submissiontype'                 => [
-                'label'            => 'Tipe Alpa',
-                'rules'            =>    'required',
-                'errors'        => [
-                    'required'    => 'Mohon mengisi {field} dahulu'
-                ]
-                ]
+        ],
+        'submissiontype'                 => [
+            'label'            => 'Tipe Alpa',
+            'rules'            =>    'required',
+            'errors'        => [
+                'required'    => 'Mohon mengisi {field} dahulu'
+            ]
+        ]
     ];
 
     public $sakit = [
@@ -1692,11 +1692,29 @@ class Validation
             ]
         ],
         'image'                 => [
-            'label'             => 'gambar',
-            'rules'             => 'uploaded[image]|max_size[image, 3024]|is_image[image]|mime_in[image,image/jpg,image/jpeg,image/png]',
+            'label'             => 'foto',
+            'rules'             => 'uploaded[image]|max_size[image, 1024]|is_image[image]|mime_in[image,image/jpg,image/jpeg,image/png]',
             'errors'            => [
                 'uploaded'      => 'Mohon upload {field} dahulu',
-                'max_size'      => 'Data {field} melebehi batas maksimum 3 Mb',
+                'max_size'      => 'Data {field} melebehi batas maksimum 1 Mb',
+                'is_image'      => 'Format file {field} salah',
+                'mime_in'       => 'Format file {field} wajib {param}',
+            ]
+        ],
+        'image2'                 => [
+            'label'             => 'foto 2',
+            'rules'             => 'max_size[image, 1024]|is_image[image]|mime_in[image,image/jpg,image/jpeg,image/png]',
+            'errors'            => [
+                'max_size'      => 'Data {field} melebehi batas maksimum 1 Mb',
+                'is_image'      => 'Format file {field} salah',
+                'mime_in'       => 'Format file {field} wajib {param}',
+            ]
+        ],
+        'image3'                 => [
+            'label'             => 'foto 3',
+            'rules'             => 'max_size[image, 1024]|is_image[image]|mime_in[image,image/jpg,image/jpeg,image/png]',
+            'errors'            => [
+                'max_size'      => 'Data {field} melebehi batas maksimum 1 Mb',
                 'is_image'      => 'Format file {field} salah',
                 'mime_in'       => 'Format file {field} wajib {param}',
             ]
