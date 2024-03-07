e3c23f96f3309a7c8e5185b2f6b5f225f0bb6ac0
Hatsu17 <keihatsu17@gmail.com>
2024-03-07 13:21:31 +0700

    Setting Auto Load PHPExcel

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -64,7 +64,9 @@ class Autoload extends AutoloadConfig
      *
      * @var array<string, string>
      */
-    public $classmap = [];
+    public $classmap = [
+        'PHPExcel' => APPPATH . 'ThirdParty/PHPExcel/PHPExcel.php'
+    ];
 
     /**
      * -------------------------------------------------------------------
