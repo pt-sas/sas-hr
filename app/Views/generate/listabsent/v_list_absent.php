c1684a1700b39e7ac5b57f9c5d78c37e1229922b
Hatsu17 <keihatsu17@gmail.com>
2024-03-21 13:25:00 +0700

    Feat : Add Filter To View Generate Alpha From List Absent

--- app/Views/generate/listabsent/v_list_absent.php
+++ app/Views/generate/listabsent/v_list_absent.php
@@ -1,25 +1,30 @@
 <?= $this->extend('backend/_partials/overview') ?>
 
 <?= $this->section('content'); ?>
+<?= $this->include($filter) ?>
 
 <div class="row">
     <div class="col-md-12">
         <div class="card">
             <div class="card-header">
-                <div class="card-body">
-                    <table class="table table-bordered table-hover table_report">
-                        <thead>
-                            <tr>
-                                <th>No</th>
-                                <th>Nik</th>
-                                <th>Nama</th>
-                                <th>Tanggal</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-                    </table>
+                <div class="float-right d-none">
+                    <?= $toolbarRealization ?>
                 </div>
             </div>
+            <div class="card-body">
+                <table class="table table-bordered table-hover table_report">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nik</th>
+                            <th>Nama</th>
+                            <th>Tanggal</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+
         </div>
     </div>
 </div>
