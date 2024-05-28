407e746c414ab63bbeacc8a2cd3acf1e48aadedf
Hatsu17 <keihatsu17@gmail.com>
2024-05-28 09:56:25 +0700

    Feat : Modify Parameter for showing data in realization overtime

--- public/custom/js/Logic.js
+++ public/custom/js/Logic.js
@@ -290,39 +290,29 @@ _tableReport.on("click", ".check-alpa", function (e) {
   const card = target.closest(".card");
   const cardHeader = card.find(".card-header");
   const floatRight = cardHeader.find(".float-right");
-  const checkbox = _tableReport
-    .rows()
-    .nodes()
-    .to$()
-    .find("input.check-alpa");
+  const checkbox = _tableReport.rows().nodes().to$().find("input.check-alpa");
 
   let checkData = [];
 
   $.each(checkbox, function (idx, item) {
-   if ($(this).is(":checked")) {
-        if ($(item).is(":checked")) checkData.push(item.value);
-        floatRight.removeClass("d-none");
-    }
-    else {
-        if (checkData.length == 0) floatRight.addClass("d-none");
+    if ($(this).is(":checked")) {
+      if ($(item).is(":checked")) checkData.push(item.value);
+      floatRight.removeClass("d-none");
+    } else {
+      if (checkData.length == 0) floatRight.addClass("d-none");
     }
   });
 });
 
 function Generate() {
-
   let formData = new FormData();
   let url = ADMIN_URL + "alpa/generate";
 
-  const checkbox = _tableReport
-    .rows()
-    .nodes()
-    .to$()
-    .find("input.check-alpa");
+  const checkbox = _tableReport.rows().nodes().to$().find("input.check-alpa");
 
   $.each(checkbox, function (idx, item) {
-   if ($(this).is(":checked")) { 
-    formData.append("trx_attendance_id[]", item.value);
+    if ($(this).is(":checked")) {
+      formData.append("trx_attendance_id[]", item.value);
     }
   });
 
@@ -347,38 +337,85 @@ function Generate() {
   });
 }
 
-// $("#form_overtime_realization_agree").on("dp.change", "#starttime, #endtime, #enddate" , function(e) {
-//   const form = $("#form_overtime_realization_agree");
-//   let startdate = new Date(form.find("[name=startdate]").val());
-//   let starttime = form.find("[name=starttime]").val();
-//   let enddate = new Date(form.find("[name=enddate]").val());
-//   let endtime = form.find("[name=endtime]").val();
-
-//   $(".datepicker").data("DateTimePicker").minDate(startdate);
-
-  
-//   console.log(startdate)
-//   console.log(starttime)
-//   console.log(enddate)
-//   console.log(endtime)
-// })
-
-_tableReport.on("click", ".btn_agree", function(e) {
+_tableReport.on("click", ".btn_agree", function (e) {
   const form = $("#form_overtime_realization_agree");
   let startdate = new Date(form.find("[name=startdate]").val());
-  maxDate = new Date(startdate.getTime() + 24 * 60 * 60 * 1000)
-  let starttime = form.find("[name=starttime]").val();
-  let enddate = new Date(form.find("[name=enddate]").val());
-  let endtime = form.find("[name=endtime]").val();
+  let date_out = new Date(form.find("[name=enddate_att]").val());
+  maxtgl = new Date(startdate.getTime() + 24 * 60 * 60 * 1000);
+  // let maxtime = new textToTime(form.find("[name=endtime_att]").val());
+  let mintime = new textToTime(form.find("[name=starttime]").val());
+  let maxtime;
 
-  if(startdate > maxDate) {
-    $(".datepicker").data("DateTimePicker").maxDate(maxDate);
-  $(".datepicker").data("DateTimePicker").minDate(startdate);
-  } else if ( startdate < maxDate) {
-    
-  $(".datepicker").data("DateTimePicker").minDate(startdate);
-  $(".datepicker").data("DateTimePicker").maxDate(maxDate);
+  if (form.find("[name=endtime_att]").val() != "") {
+    maxtime = new textToTime(form.find("[name=endtime_att]").val());
+  } else {
+    maxtime = new textToTime("05:00");
   }
+  if (form.is($("#form_overtime_realization_agree"))) {
+    /**
+     * Logic for refresh and set min & max value of Date
+     */
+    $(".datepicker").data("DateTimePicker").maxDate(false).minDate(false);
+    $(".timepicker-end").data("DateTimePicker").maxDate(false).minDate(false);
 
-  console.log(startdate);
-})
+    // Check if Data valid in Date Out and Clock Out
+    if (date_out != "Invalid Date") {
+      $(".datepicker")
+        .data("DateTimePicker")
+        .maxDate(date_out)
+        .minDate(startdate);
+      if (maxtime != "Invalid Date") {
+        $(".timepicker-end")
+          .data("DateTimePicker")
+          .maxDate(maxtime)
+          .minDate(mintime);
+      }
+    } else {
+      $(".datepicker")
+        .data("DateTimePicker")
+        .maxDate(maxtgl)
+        .minDate(startdate);
+      $(".timepicker-end")
+        .data("DateTimePicker")
+        .maxDate(false)
+        .minDate(mintime);
+    }
+
+    // This logic for set mix and max time base on enddate changes
+    $(".datepicker").on("dp.change", function (e) {
+      let dateend = new Date(e.date);
+
+      form.find("[name=endtime_realization]").val("");
+
+      if (startdate < dateend) {
+        $(".timepicker-end")
+          .data("DateTimePicker")
+          .maxDate(false)
+          .minDate(false);
+        $(".timepicker-end")
+          .data("DateTimePicker")
+          .maxDate(maxtime)
+          .minDate(false);
+      } else {
+        $(".timepicker-end")
+          .data("DateTimePicker")
+          .maxDate(false)
+          .minDate(false);
+        $(".timepicker-end")
+          .data("DateTimePicker")
+          .maxDate(false)
+          .minDate(mintime);
+      }
+    });
+  }
+});
+
+//** Function for convert Time String to Date**/
+
+function textToTime(time) {
+  const [hours, minutes] = time.split(":");
+  // const date = new Date(date1);
+  const date = new Date();
+  date.setHours(Number(hours), Number(minutes), 0);
+  return date;
+}
