187002fdd76b645e7c921d7a2f8b9d6e56cafeec
Hatsu17 <keihatsu17@gmail.com>
2024-07-11 11:31:06 +0700

    Refactor : Change menu name & value code

--- app/Views/masterdata/employee/form_employee.php
+++ app/Views/masterdata/employee/form_employee.php
@@ -217,20 +217,20 @@
         </div>
         <div class="col-md-3">
             <div class="form-group">
-                <label for="md_position_id">Posisi <span class="required">*</span></label>
+                <label for="md_position_id">Jabatan <span class="required">*</span></label>
                 <select class="form-control select-data" id="md_position_id" name="md_position_id"
                     data-url="position/getList">
-                    <option value="">Select Posisi</option>
+                    <option value="">Select Jabatan</option>
                 </select>
                 <small class="form-text text-danger" id="error_md_position_id"></small>
             </div>
         </div>
         <div class="col-md-3">
             <div class="form-group">
-                <label for="md_levelling_id">Jabatan <span class="required">*</span></label>
+                <label for="md_levelling_id">Level <span class="required">*</span></label>
                 <select class="form-control select-data" id="md_levelling_id" name="md_levelling_id"
                     data-url="levelling/getList">
-                    <option value="">Select Jabatan</option>
+                    <option value="">Select Level</option>
                 </select>
                 <small class="form-text text-danger" id="error_md_levelling_id"></small>
             </div>
