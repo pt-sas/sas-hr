fa8f810bdf3dcebfae78919eb4ab4b2a5d323ae3
Hatsu17 <keihatsu17@gmail.com>
2024-08-01 11:01:25 +0700

    Fix : Fixing some Validation and Form for Attendance Realization

--- app/Views/transaction/attendancerealization/modal_realization.php
+++ app/Views/transaction/attendancerealization/modal_realization.php
@@ -1,17 +1,17 @@
-<div class="modal fade" id="modal_realization_agree">
+<div class="modal fade" id="modal_attendance_realization_agree">
     <div class="modal-dialog modal-md">
         <div class="modal-content">
             <div class="modal-header">
                 <h4 class="modal-title">Realisasi</h4>
             </div>
             <div class="modal-body" id="realization">
-                <form class="form-horizontal" id="form_realization_agree">
+                <form class="form-horizontal" id="form_attendance_realization_agree">
                     <div class="row">
                         <div class="col-md-12">
                             <div class="form-group">
                                 <label for="submissiondate">Tanggal Tidak Lengkap<span class="required">*</span></label>
                                 <div class="input-icon">
-                                    <input type="text" class="form-control datepicker" name="submissiondate" readonly>
+                                    <input type="text" class="form-control datepicker" name="submissiondate" disabled>
                                     <span class="input-icon-addon">
                                         <i class="fa fa-calendar"></i>
                                     </span>
@@ -21,11 +21,35 @@
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
+                                <label for="enddate_att">Tanggal Absen</label>
+                                <div class="input-icon">
+                                    <input type="text" class="form-control" name="enddate_att" id="enddate_att"
+                                        disabled>
+                                    <span class="input-icon-addon">
+                                        <i class="fa fa-calendar"></i>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="endtime_att">Jam Absen</label>
+                                <div class="input-icon">
+                                    <input type="text" class="form-control" name="endtime_att" id="endtime_att"
+                                        disabled>
+                                    <span class=" input-icon-addon">
+                                        <i class="fa fa-clock"></i>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
                                 <label for="enddate_realization">Tanggal Realisasi <span
                                         class="required">*</span></label>
                                 <div class="input-icon">
                                     <input type="text" class="form-control datepicker" name="enddate_realization"
-                                        id="enddate_realization">
+                                        id="enddate_realization" disabled>
                                     <span class="input-icon-addon">
                                         <i class="fa fa-calendar"></i>
                                     </span>
@@ -63,7 +87,7 @@
     </div>
 </div>
 
-<div class="modal fade" id="modal_realization_not_agree">
+<div class="modal fade" id="modal_realization_attendance_not_agree">
     <div class="modal-dialog modal-md">
         <div class="modal-content">
             <div class="modal-header">
