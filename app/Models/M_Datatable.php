f7a68e182fbf7a362456b606604a72537367de79
Hatsu17 <keihatsu17@gmail.com>
2024-10-03 11:14:27 +0700

    Feat : Add condition function afterupdate for exit interview

--- app/Models/M_Datatable.php
+++ app/Models/M_Datatable.php
@@ -244,6 +244,11 @@ class M_Datatable extends Model
                 $model = new M_EmployeeDeparture($this->request);
                 $model->doAfterUpdate($rows);
             }
+
+            if ($this->table === "trx_interview") {
+                $model = new M_Interview($this->request);
+                $model->doAfterUpdate($rows);
+            }
         } catch (\Exception $e) {
             throw new \RuntimeException($e->getMessage(), $e->getCode(), $e);
         }
