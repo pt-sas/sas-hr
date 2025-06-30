a24992a4c9674287c4d573c4f6240b54803eaec8
Hatsu17 <keihatsu17@gmail.com>
2025-06-30 09:43:01 +0700

    Feat : Adding Function get Emp Work Detail

--- app/Models/M_EmpWorkDay.php
+++ app/Models/M_EmpWorkDay.php
@@ -39,4 +39,17 @@ class M_EmpWorkDay extends Model
 		$this->request = $request;
 		$this->builder = $this->db->table($this->table);
 	}
+
+	public function getEmpWorkDetail($where)
+	{
+		$this->builder->select('md_work_detail.*,
+			md_day.name as day,
+            md_work.name as work');
+
+		$this->builder->join('md_work', 'md_work.md_work_id = ' . $this->table . '.md_work_id', 'inner');
+		$this->builder->join('md_work_detail', 'md_work_detail.md_work_id = md_work.md_work_id', 'inner');
+		$this->builder->join('md_day', 'md_day.md_day_id = md_work_detail.md_day_id', 'left');
+		$this->builder->where($where);
+		return $this->builder->get();
+	}
 }
