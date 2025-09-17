9655e1d02ce451109e70033927b46349e13baac2
Hatsu17 <keihatsu17@gmail.com>
2025-09-17 10:06:41 +0700

    Fix : Fixing Logic Report Allowance

--- app/Views/report/allowance/v_report_allowance.php
+++ app/Views/report/allowance/v_report_allowance.php
@@ -6,59 +6,26 @@
         <div class="card">
             <div class="card-body">
                 <form action="<?= site_url('sas/laporan-saldo-tkh/reportAll') ?>" method="POST" id="parameter_report">
-                    <!-- <div class="form-group row">
-                        <label for="md_division_id" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Cabang </label>
-                        <div class="col-lg-6 col-md-9 col-sm-8 select2-input select2-primary">
-                            <select class="form-control multiple-select-branch" name="md_branch_id[]"></select>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="md_division_id" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Divisi </label>
-                        <div class="col-lg-6 col-md-9 col-sm-8 select2-input select2-primary">
-                            <select class="form-control multiple-select-division" name="md_division_id[]"></select>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="md_employee_id" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Karyawan </label>
-                        <div class="col-lg-6 col-md-9 col-sm-8 select2-input select2-primary">
-                            <select class="form-control multiple-select-employee" name="md_employee_id[]"></select>
-                        </div>
-                    </div> -->
-                    <!-- <div class="form-group row">
-                        <label for="md_employee_id" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Karyawan </label>
-                        <div class="col-lg-6 col-md-9 col-sm-8 select2-input select2-primary">
-                            <select class="form-control multiple-select-employee" name="md_employee_id[]"></select>
-                        </div>
-                    </div> -->
                     <div class="form-group row">
                         <label for="md_division_id" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Cabang
                         </label>
                         <div class="col-lg-6 col-md-9 col-sm-8 select2-input select2-primary">
-                            <select class="form-control multiple-select" name="md_branch_id" multiple="multiple"
-                                style="width: 100%;">
-                                <?php foreach ($ref_branch as $row) : ?>
-                                    <option value="<?= $row->md_branch_id ?>"><?= $row->name ?></option>
-                                <?php endforeach ?>
-                            </select>
+                            <select class="form-control multiple-select-branch access" name="md_branch_id[]"></select>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="md_division_id" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Divisi
                         </label>
                         <div class="col-lg-6 col-md-9 col-sm-8 select2-input select2-primary">
-                            <select class="form-control multiple-select" name="md_division_id" multiple="multiple"
-                                style="width: 100%;">
-                                <?php foreach ($ref_division as $row) : ?>
-                                    <option value="<?= $row->md_division_id ?>"><?= $row->name ?></option>
-                                <?php endforeach ?>
-                            </select>
+                            <select class="form-control multiple-select-division access"
+                                name="md_division_id[]"></select>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="md_employee_id" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Karyawan
                         </label>
                         <div class="col-lg-6 col-md-9 col-sm-8 select2-input select2-primary">
-                            <select class="form-control multiple-select" name="md_employee_id" multiple="multiple"
+                            <select class="form-control multiple-select" name="md_employee_id[]" multiple="multiple"
                                 style="width: 100%;">
                                 <?php foreach ($ref_employee as $row) : ?>
                                     <option value="<?= $row->md_employee_id; ?>"><?= $row->value; ?></option>
