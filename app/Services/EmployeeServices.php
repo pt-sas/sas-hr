fad852e38a556f7b238ad4778d9c36a4ebd85025
Alvine <keihatsu17@gmail.com>
2026-04-30 16:04:45 +0700

    Fix : Rename API Employee Detail to My Profile

--- app/Services/EmployeeServices.php
+++ app/Services/EmployeeServices.php
@@ -4,16 +4,22 @@ namespace App\Services;
 
 use App\Exceptions\NotFoundException;
 use App\Models\M_Branch;
+use App\Models\M_Country;
 use App\Models\M_Division;
 use App\Models\M_EmpBranch;
 use App\Models\M_EmpDivision;
 use App\Models\M_Employee;
 use App\Models\M_Levelling;
 use App\Models\M_Position;
+use App\Models\M_Province;
+use App\Models\M_Role;
 use App\Models\M_Status;
+use App\Models\M_SubDistrict;
+use App\Models\M_User;
 
 class EmployeeServices extends BaseServices
 {
+    protected $PATH_Karyawan = "karyawan";
 
     public function __construct(int $userID, int $employeeID)
     {
@@ -27,7 +33,7 @@ class EmployeeServices extends BaseServices
         $this->entity = new \App\Entities\Employee();
     }
 
-    public function getEmployeeDetail(int $employeeId)
+    public function getProfile()
     {
         //* call Model
         $mEmpBranch = new M_EmpBranch($this->request);
@@ -37,22 +43,28 @@ class EmployeeServices extends BaseServices
         $mLevelling = new M_Levelling($this->request);
         $mPosition = new M_Position($this->request);
         $mStatus = new M_Status($this->request);
+        $mUser = new M_User($this->request);
+        $mRole = new M_Role($this->request);
 
-        $fieldsAllowed = [
-            'md_employee_id',
-            'value',
-            'nickname',
-            'fullname',
-            'nik',
-            'isactive',
-            'md_levelling_id',
-            'md_position_id',
-            'md_status_id',
-            'superior_id'
-        ];
+        //* Get Employee Data
+        $this->model->select("{$this->model->table}.*,
+        rd.name as gender_detail,
+        CONCAT_WS(', ',
+        {$this->model->table}.address_dom,
+        sd.name,
+        d.name,
+        city.name,
+        p.name,
+        c.name) AS full_address");
 
-        $employee = $this->model->select($fieldsAllowed)
-            ->where($this->model->primaryKey, $employeeId)
+        $this->model->join('md_country c', "c.md_country_id = {$this->model->table}.md_country_dom_id", 'left');
+        $this->model->join('md_province p', "p.md_province_id = {$this->model->table}.md_province_dom_id", 'left');
+        $this->model->join('md_city city', "city.md_city_id = {$this->model->table}.md_city_dom_id", 'left');
+        $this->model->join('md_district d', "d.md_district_id = {$this->model->table}.md_district_dom_id", 'left');
+        $this->model->join('md_subdistrict sd', "sd.md_subdistrict_id = {$this->model->table}.md_subdistrict_dom_id", 'left');
+        $this->model->join('sys_ref_detail rd', "rd.value = {$this->model->table}.gender AND rd.sys_reference_id = 5", 'left');
+
+        $employee = $this->model->where($this->model->primaryKey, $this->employeeID)
             ->first();
 
         if (!$employee)
@@ -62,7 +74,7 @@ class EmployeeServices extends BaseServices
         $branchIDs = array_column(
             $mEmpBranch
                 ->select($mBranch->primaryKey)
-                ->where('md_employee_id', $employeeId)
+                ->where('md_employee_id', $this->employeeID)
                 ->findAll(),
             $mBranch->primaryKey
         );
@@ -70,7 +82,7 @@ class EmployeeServices extends BaseServices
         $divIDs = array_column(
             $mEmpDiv
                 ->select($mDiv->primaryKey)
-                ->where('md_employee_id', $employeeId)
+                ->where('md_employee_id', $this->employeeID)
                 ->findAll(),
             $mDiv->primaryKey
         );
@@ -78,45 +90,71 @@ class EmployeeServices extends BaseServices
         //* Mapping detail data
         $branches = $branchIDs
             ? array_map(fn($row) => [
-                'id'   => $row->getBranchId(),
-                'name' => $row->getName()
+                'id'   => (int) $row->getBranchId(),
+                'name' => ucwords(strtolower($row->getName()))
             ], $mBranch->whereIn($mBranch->primaryKey, $branchIDs)->findAll())
             : [];
 
         $divisions = $divIDs
             ? array_map(fn($row) => [
-                'id'   => $row->getDivisionId(),
+                'id'   => (int) $row->getDivisionId(),
                 'name' => $row->getName()
             ], $mDiv->whereIn($mDiv->primaryKey, $divIDs)->findAll())
             : [];
 
         $leveling = array_map(fn($row) => [
-            'id'   => $row->getLevellingId(),
+            'id'   => (int) $row->getLevellingId(),
             'name' => $row->getName()
         ], $mLevelling->where($mLevelling->primaryKey, $employee->md_levelling_id)->findAll());
 
         $position = array_map(fn($row) => [
-            'id'   => $row->getPositionId(),
+            'id'   => (int) $row->getPositionId(),
             'name' => $row->getName()
         ], $mPosition->where($mPosition->primaryKey, $employee->md_position_id)->findAll());
 
         $status = array_map(fn($row) => [
-            'id'   => $row->getStatusId(),
-            'name' => $row->getName()
+            'id'   => (int) $row->getStatusId(),
+            'name' => ucwords(strtolower($row->getName()))
         ], $mStatus->where($mStatus->primaryKey, $employee->md_status_id)->findAll());
 
         $superior = array_map(fn($row) => [
-            'id'   => $row->getEmployeeId(),
-            'name' => $row->getFullname()
+            'id'   => (int) $row->getEmployeeId(),
+            'name' => ucwords(strtolower($row->getFullname()))
         ], $this->model->where($this->model->primaryKey, $employee->superior_id)->findAll());
 
-        $employee->md_branch_id  = $branches;
-        $employee->md_division_id = $divisions;
-        $employee->md_levelling_id = $leveling;
-        $employee->md_position_id = $position;
-        $employee->md_status_id = $status;
-        $employee->superior_id = $superior;
+        $userRole = $mUser->detail(['sys_user.md_employee_id' => $this->employeeID])->getRow();
 
-        return [$employee];
+        $role = array_map(fn($row) => [
+            'id'   => (int) $row->getRoleId(),
+            'name' => $row->getName()
+        ], $mRole->where($mRole->primaryKey, $userRole->role)->findAll());
+
+        $dataEntity = new \App\Entities\Employee();
+
+        $dataEntity->md_employee_id = $this->employeeID;
+        $dataEntity->fullname = $employee->fullname;
+        $dataEntity->nickname = $employee->nickname;
+        $dataEntity->value = $employee->value;
+        $dataEntity->nik = $employee->nik;
+        $dataEntity->gender = $employee->gender_detail;
+        $dataEntity->pob = $employee->pob;
+        $dataEntity->birthday = format_idn(date('Y-m-d', strtotime($employee->birthday)));
+        $dataEntity->email = $employee->email;
+        $dataEntity->phone = $employee->phone;
+        $dataEntity->telegram_username = $employee->telegram_username;
+        $dataEntity->registerdate = format_idn(date('Y-m-d', strtotime($employee->registerdate)));;
+        $dataEntity->officephone = $employee->officephone;
+        $dataEntity->officeemail = !empty($userRole) ? $userRole->email : "";
+        $dataEntity->address = $employee->full_address;
+        $dataEntity->md_branch_id  = $branches;
+        $dataEntity->md_division_id = $divisions;
+        $dataEntity->md_levelling_id = $leveling;
+        $dataEntity->md_position_id = $position;
+        $dataEntity->md_status_id = $status;
+        $dataEntity->superior_id = $superior;
+        $dataEntity->sys_role_id = $role;
+        $dataEntity->image = '/uploads/' . $this->PATH_Karyawan . '/' . $employee->image;
+
+        return [$dataEntity];
     }
 }
